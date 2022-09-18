48f15e9e765557b4989818608664c9f2d57bc0a5
Petr Cibulka <cibulka.me@gmail.com>
2022-09-18 16:08:59 +0200

    fix: fighting the purge on Vercel

--- src/pages/api/blog.ts
+++ src/pages/api/blog.ts
@@ -1,4 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import path from 'path';
+import fs from 'fs';
 
 import getPostOnServer from 'src/helpers/getPostOnServer';
 import { BlogPost } from 'src/types';
@@ -11,9 +13,14 @@ export default function getBlog(
   if (!index) throw new Error('Need page number');
 
   try {
+    /* If we don't do this, Vercel removes `db` folder which results to ENOENT error */
+    const purgePath = path.join(process.cwd(), 'db');
+    fs.readFileSync(purgePath + '/dontpurgeme.json');
+    /* End of preventing purge */
+
     const post = getPostOnServer(index);
     res.status(200).send(post);
   } catch (e) {
-    res.status(500).send({ error: (e as any)?.message || 'Unknown error' });
+    res.status(500).send({ error: (e as any)?.toString() || 'Unknown error' });
   }
 }
